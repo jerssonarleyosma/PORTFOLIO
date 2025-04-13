04958e617571e81a4407d8b91482ebea1ee16a46
jerssonarleyosma <jerssonarleyosma@gmail.com>
2025-04-13 01:50:05 -0500

    feat(proyect): add dist dir for build and ignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PORTFOLIO/', // MUY IMPORTANTE para GitHub Pages
   plugins: [react()],
   server: {
     host: 'localhost', // Escuchar el local host para escuchar en todas las interfaces es con 0.0.0.0  
